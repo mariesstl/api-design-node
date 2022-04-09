89e16c698eaab62fd4685b03f69d1c28944e8d5e
mariesstl <judybat25@gmail.com>
2022-04-09 18:43:45 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 // TODO: make this work.
-// if yuo go to localhost:3000 the app
+// if you go to localhost:3000 the app
 // there is expected crud to be working here
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var _ = require('lodash');
+const { json } = require('body-parser');
 
 // express.static will serve everything
 // with in client as a static resource
@@ -22,6 +23,43 @@ var lions = [];
 var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+
+app.listen(3000);
+console.log('on port 3000');
+
+app.get('/lions', function(req,res){
+    res.json(lions);
+});
+
+app.get('/lions/:id', function(req,res){
+    var lion = _.find(lions,{id: req.params.id});
+    res.json(lion || {}); //Would have to check if the object is empty on the front end.
+});
+
+app.post('/lions', function(req, res){
+   var lion = req.body;
+   id++;
+   lion.id = id + '' ;
+   
+   lions.push(lion);
+   
+   res.json(lion);
+});
+
+app.put('/lions/:id', function(req,res){
+    var update = req.body; //safety check to make sure you don't delete the id
+    if (update.id){
+        delete update.id;
+    }
+   
+   var lion = _.findIndex(lions, {id: req.params.id});
+   if (!lions[lion]){
+        res.send();
+    }else{
+        var updatedLion = _.assign(lions[lion], update);
+        res.json(updatedLion);
+    }
+});
 
 app.listen(3000);
 console.log('on port 3000');
