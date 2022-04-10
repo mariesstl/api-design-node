61145358cdacd5a7e5b64e6f04bc5f47dfc413e2
mariesstl <judybat25@gmail.com>
2022-04-09 22:52:08 -0500

    Update path to client

--- server/server.js
+++ server/server.js
@@ -8,11 +8,12 @@ var lionRouter = require('./lions');
 var tigerRouter = require('./tigers');
 
 app.use(morgan('dev'))
-app.use(express.static('client'));
+app.use(express.static('../client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 // this is called mounting. when ever a req comes in for
 // '/lion' we want to use this router
+
 app.use('/lions', lionRouter);
 app.use('/tigers', tigerRouter);
 
