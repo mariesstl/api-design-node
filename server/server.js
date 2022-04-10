5fa1f0bb9e63b865e99e9af40f564d1bb3da805b
mariesstl <judybat25@gmail.com>
2022-04-10 11:01:47 -0500

    Notes and spelling corrections

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 var api = require('./api/api'); //this is a just a router called api
 
-// setup the app middlware
+// setup the app middlware when provided an app.
 require('./middleware/appMiddlware')(app);
 
 // setup the api
-app.use('/api/', api);
+app.use('/api', api);
 // set up global error handling
 
 // export the app for testing
