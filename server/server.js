e6f9813817c6bb16a8412f8a9acf00abfc646b06
mariesstl <judybat25@gmail.com>
2022-04-09 22:09:39 -0500

    Add tigerRouter

--- server/server.js
+++ server/server.js
@@ -10,12 +10,13 @@ var lionRouter = require('./lions');
 var tigerRouter = require('./tigers');
 
 app.use(morgan('dev'))
-app.use(express.static('client'));
+app.use(express.static('../client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 // this is called mounting. when ever a req comes in for
 // '/lion' we want to use this router
 app.use('/lions', lionRouter);
+app.use('/tigers', tigerRouter);
 
 app.use(function(err, req, res, next) {
   if (err) {
